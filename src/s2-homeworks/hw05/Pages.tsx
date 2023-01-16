21e697abc89b8d2d9a1f09b78454735c0f245662
Keizt <keyzzzt@gmail.com>
2023-01-16 20:28:54 +0200

    '...'

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -6,9 +6,9 @@ import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
 
 export const PATH = {
-    PRE_JUNIOR: '/pre-junior',
-    JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus',
+    PRE_JUNIOR: '/pre-junior-link',
+    JUNIOR: '/junior-link',
+    JUNIOR_PLUS: '/junior-plus-link',
 }
 
 function Pages() {
