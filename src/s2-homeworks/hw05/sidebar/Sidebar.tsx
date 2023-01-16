1f0674eac35b679104e7c1e1e9a33a41b5c0c34f
Keizt <keyzzzt@gmail.com>
2023-01-16 19:48:10 +0200

    'HW5'

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react'
+import React, {FC, useState} from 'react'
 import {NavLink} from 'react-router-dom'
 import s from './Sidebar.module.css'
 import {PATH} from '../Pages'
@@ -12,6 +12,7 @@ type PropsType = {
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
+
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -31,7 +32,9 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive }) =>
+                            isActive ? s.active : undefined
+                        }
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +42,9 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive }) =>
+                            isActive ? s.active : undefined
+                        }
                     >
                         Junior
                     </NavLink>
@@ -47,9 +52,11 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive }) =>
+                            isActive ? s.active : undefined
+                        }
                     >
-                        Junior Plus
+                        Junior PLUS
                     </NavLink>
                 </nav>
             </aside>
